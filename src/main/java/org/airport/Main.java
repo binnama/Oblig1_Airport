9483a07a17131c3315343bbcc8afe93866e448c5
binnama <laasestad@yahoo.no>
2023-01-20 15:53:34 +0100

    adding stuff

--- src/main/java/org/airport/Main.java
+++ src/main/java/org/airport/Main.java
@@ -1,7 +1,10 @@
 package org.airport;
 
+import org.airport.module.Airport;
+
 public class Main {
     public static void main(String[] args) {
 
+        new Airport();
     }
 }
--- src/main/java/org/airport/module/Airport.java
+++ src/main/java/org/airport/module/Airport.java
@@ -5,46 +5,114 @@ import java.util.*;
 
 public class Airport {
 
+    int runwayFree;
     int maxServiceTime;
     int maxTicks;
     float planeRatio;
     
     public airport(String name) {
-        Scanner scanner = new Scanner(System.in); scanner = new Scanner(System.in); scanner = new Scanner(System.in);
-            System.out.println("Welcome to " + name + "airport");
-            System.out.println("How long should the airport be open: ");
-            maxTicks = scanner.nextInt();
-            System.out.println("Average number of plane each hour: ");
-            planeRatio = scanner.nextFloat();        
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Welcome to " + name + "airport");
+        System.out.println("How long should the airport be open: ");
+        maxTicks = scanner.nextInt();
+        System.out.println("Average number of plane each hour: ");
+        planeRatio = scanner.nextFloat();
     }
-
+    // Saves when the plane arrives
     private class airplane {
+        private int arrival;
+        public airplane (int arrival) {
+            this.arrival = arrival;
+        }
+        public int queueTime (int time) {
+            return time - arrival;
+        }
+    }
+    // Saves when the runway is available
+    private class runway {
+        private int planeTime; 
+        public runway() {
+            planeTime = 0;
+        }
+        public void setPlaneTime(int time) {
+            planeTime = time;
+        }
+        public boolean isAvailable(int time) {
+            return time >= planeTime;
+        }
+    }
+
+    private class landingQueue {
+        private int landingTime;
+        public landingQueue () {
+            landingTime = 0;
+        }
+        public void setPlaneTime(int time) {
+            landingTime = time;
+        }
+    }
+
+    private class takeoffQueue {
+        private int takeoffTime;
+        public takeoffQueue () {
+            takeoffTime = 0;
+        }
+        public void setPlaneTime(int time) {
+            takeoffTime = time;
+        }
+    }
+
+    public void simulate() {
         
+        int sumPlaneTime    = 0; // Total waitingtime to use the runway
+        int sumPlanes       = 0; // Sum all plains entering the airport
+        int sumRunwayFree   = 0; // All the times the runway was free
+        
+        // Making a queue
+        Queue<landingQueue> lq = new LinkedList<landingQueue>();
+        Queue<takeoffQueue> tq = new LinkedList<takeoffQueue>();
+
+
+        // Makes an array for the runway.
+        runway[] open = new runway[runwayFree];
+
+        // Initates empty runway
+        for (int i = 0; i < runwayFree; i++) {
+            open[i] = new runway();
+        }
+
+        Random R = new Random();
+
+        // Simulating for each tick
+        for (int time = 0; time < maxTicks; i++) {
+
+            if(R.nextDouble() < planeRatio) {
+                
+            }
+          
+            if (mean < planeRatio) {
+                runwayQueue.add();
+            }
+
+
+            for (int i = 0; i < lq && i < tq; i++) {
+                if (landing[i].isAvailable(time)) {
+                    if (!landing.isEmpty()) {
+                        takeoff to = takeoff.remove();
+                        
+                        // Markes bussy runway
+                        runway.setPlaneTime(time);
+                        sumPlaneTime += to.queueTime(time);
+                        sumPlanes++;
+                    }
+                    else if (!landing !=) {
+                        
+                    }
+                }
+            }
+        }
     }
 
-    public Airport() {
-        createQueue();
-        createLanding();
-        createTakeoff();
-    }
-
-    public static Integer createQueue() {
-        int[] queue = {};
-
-    }
-
-    public static Integer createLanding() {
-        int b = 2;
-        return b;
-    }
-
-    public static void createTakeoff() {
-        int three = 3;
-    }
-
-    // 1 klasse selv om det skal være 2 køer
-    //  -ankomstTid, -
-    // 1 rullebane
     private static int getPoissonRandom(double mean)
     {
         Random r = new Random();
